2bd1fa7648a9768663e8b507f6a50cb1a2fe13b3
iiTzKahrba <40093910+iiTzKahrba@users.noreply.github.com>
2018-07-09 22:28:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,7 +59,6 @@ let welcomer = member.guild.channels.find("name","welcome");
         .setAuthor(h.username,h.avatarURL)
         .addField(': **تاريخ دخولك الدسكورد **',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
          .addField(':** تاريخ دخولك السيرفر **',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
-         .addField(':** Invited By : ** ${Invite.inviter} **')      
          .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:norelden});          
 
