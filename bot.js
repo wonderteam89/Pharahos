f6f4046ce9a985b366e80605f64f57b8d90bb01b
iiTzKahrba <40093910+iiTzKahrba@users.noreply.github.com>
2018-07-09 22:25:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,70 +63,10 @@ let welcomer = member.guild.channels.find("name","welcome");
          .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:norelden});          
 
-      var Canvas = require('canvas')
-      var jimp = require('jimp')
-      
-      const w = ['./w1.png',
-      './w2.png',
-      './w3.png',
-      './w4.png',
-      './w5.png',
-      './w7.png',
-      './w8.png'];
-	      
-              let Image = Canvas.Image,
-                  canvas = new Canvas(401, 202),
-                  ctx = canvas.getContext('2d');
-              ctx.patternQuality = 'bilinear';
-              ctx.filter = 'bilinear';
-              ctx.antialias = 'subpixel';
-              ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-              ctx.shadowOffsetY = 2;
-              ctx.shadowBlur = 2;
-              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-                  if (err) return console.log(err)
-                  let BG = Canvas.Image;
-                  let ground = new Image;
-                  ground.src = Background;
-                  ctx.drawImage(ground, 0, 0, 401, 202);
-      
-      })
-      
-                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
-                      jimp.read(url, (err, ava) => {
-                          if (err) return console.log(err);
-                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                              if (err) return console.log(err);
-      
-                              //AVATARً
-                              let Avatar = Canvas.Image;
-                              let ava = new Avatar;
-                              ava.src = buf;
-                              ctx.drawImage(ava, 152, 27, 95, 95);
-                              
-                                                      //wl
-                              ctx.font = '20px Arial Bold';
-                              ctx.fontSize = '20px';
-                              ctx.fillStyle = "#FFFFFF";
-                              ctx.textAlign = "center";
-                                                         ctx.fillText(member.user.username, 200, 154);
-                              
-                              //NAMEً
-                              ctx.font = '20px Arial';
-                              ctx.fontSize = '28px';
-                              ctx.fillStyle = "#FFFFFF";
-                              ctx.textAlign = "center";
-      ctx.fillText(`انت العضو رقم  ${member.guild.memberCount} !! `
-                              , 200, 190);
-    welcomer.sendFile(canvas.to1Buffer())
-      
-      
-      
-      })
-      })
-      
       }
       });
+
+
   client.on('message', message => {
     if (!message.guild) return; 
     if (message.content.startsWith("رابط")) {
