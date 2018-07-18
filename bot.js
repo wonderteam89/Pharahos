6671e97660e15f3380a2efdc8973f3fa0762edbc
iiTzKahrba <40093910+iiTzKahrba@users.noreply.github.com>
2018-07-18 19:24:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,6 @@ const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
-const client = new Discord.Client();
 const adminprefix = "k!";
 const devs = ['286088294234718209'];
 const prefix = '-'
